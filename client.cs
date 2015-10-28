93d0c388d64814454a00095ad9aa4347398c3211
TheBlackParrot <theblackparrot0@gmail.com>
2015-10-28 01:21:09 -0500

    loading freezes on servers with RTB, fixed

--- client.cs
+++ client.cs
@@ -5,7 +5,7 @@ exec("./overrides.cs");
 
 package BLPrefClientPackage {
 	function GameConnection::setConnectArgs(%a, %b, %c, %d, %e, %f, %g, %h, %i, %j, %k, %l, %m, %n, %o,%p) {
-		Parent::setConnectArgs(%a, %b, %c, %d, %e, %f, $BLPrefs::Version, %h, %i, %j, %k, %l, %m, %n, %o, %p);
+		Parent::setConnectArgs(%a, %b, %c, %d, %e, %f, %g, $BLPrefs::Version, %i, %j, %k, %l, %m, %n, %o, %p);
 	}
 };
 activatePackage(BLPrefClientPackage);
